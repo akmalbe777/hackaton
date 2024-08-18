2450ecb8568e3f7ecb19f93b80df7e4ed05301a0
Hydra Coder <lazizbekabdullayev118@gmail.com>
2024-08-18 14:04:14 +0500

    das

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,5 @@ import card from './card.png'
 import logo from './logo.png'
 import logotext from './logotext.png'
 import vk from './vk.png'
-export{top, card, logo, logotext, vk}
+import tg from './tg.png'
+export{top, card, logo, logotext, vk, tg}
