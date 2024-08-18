de2a6c10a039250370b1ef1f29d53a60b58a55f1
Hydra Coder <lazizbekabdullayev118@gmail.com>
2024-08-18 11:24:57 +0500

    fghj

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { Box, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react'
+import { Box, Checkbox, CheckboxGroup, Collapse, Flex, Heading, IconButton, Radio, RadioGroup, Select, Stack, Text, useDisclosure } from '@chakra-ui/react'
 import {
     Accordion,
     AccordionItem,
@@ -8,12 +8,69 @@ import {
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import Sidebar from '../Sidebar'
+import { SlArrowUp } from "react-icons/sl";
 
 export default function Main() {
     const [value, setValue] = useState('1')
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleFocus = () => {
+        setIsOpen(true);
+    };
+
+    const handleBlur = () => {
+        setIsOpen(false);
+    };
+    const { isOpen: isOpen1, onToggle: onOpen1 } = useDisclosure();
+  const [value1, setValue1] = useState('all');
+
     return (
         <Box px='120px'>
-           <Sidebar />
+            <Box width="100%" padding="10px" backgroundColor="white">
+                <Flex justifyContent="space-between" alignItems="center">
+                    {/* Number of products found */}
+                    <Text>Найдено товаров: 245</Text>
+
+                    {/* Radio buttons */}
+                    <RadioGroup onChange={setValue} value={value} colorScheme="red">
+                        <Stack direction="row" spacing={5}>
+                            <Radio value="1">Все товары</Radio>
+                            <Radio value="2">Новинки</Radio>
+                            <Radio value="3">Со скидкой</Radio>
+                        </Stack>
+                    </RadioGroup>
+
+                    {/* Sorting dropdown */}
+                    <Select
+                        placeholder={isOpen ? '' : 'По популярности'}
+                        icon={<SlArrowUp size={'5'} />}
+                        iconSize="24px"
+                        iconColor="black"
+                        border="none"
+                        width="200px"
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    >
+                        <option value="option1">Сначала дешевле</option>
+                        <option value="option2">Сначала дороже</option>
+                    </Select>
+                </Flex>
+            </Box>
+            <Sidebar />
+
+        {/* <RadioGroup onChange={setValue1} value={value1}>
+          <Stack direction="column">
+            <Radio value="all">Все казаны</Radio>
+            <Checkbox value="4.5">4,5 литра</Checkbox>
+            <Checkbox value="5">5 литров</Checkbox>
+            <Checkbox value="6">6 литров</Checkbox>
+            <Checkbox value="7">7 литров</Checkbox>
+            <Checkbox value="8">8 литров</Checkbox>
+            <Checkbox value="10">10 литров</Checkbox>
+            <Checkbox value="12">12 литров</Checkbox>
+          </Stack>
+        </RadioGroup> */}
+            
         </Box>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,101 @@
-import { Accordion, AccordionButton, AccordionItem, Box, Text } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Button, Image, Input, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Text } from '@chakra-ui/react'
 import React from 'react'
+import { top } from '../assets'
+import { AiOutlineMinus } from "react-icons/ai";
 
 export default function Sidebar() {
+    const [value, setValue] = React.useState([1250, 12500]);
     return (
-        <Box background='white'>
-            <Text>Все товары</Text>
-            <Text>Афганские скороварки</Text>
-            <Text>Казаны и котлы</Text>
-            <Text>Учаги</Text>
-            <Text>Кастрюли</Text>
-            <Text>Мантоварки</Text>
-            <Text>Сковороды</Text>
-            <Text>Термосы</Text>
-            <Text>Чайники и френч-прессы</Text>
-            <Text>Сервировочная посуда</Text>
-            <Text>Столовые приборы</Text>
-            <Text>Бытовая техника</Text>
-            <Text>Бытовая химия</Text>
-            <Text>Товары для дома и сада</Text>
-            <Accordion>
+        <Box background='white' width='250px'>
+            <Box pl='20px' display='flex' flexDir='column' gap='8px'>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Все товары</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Афганские скороварки</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Казаны и котлы</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Учаги</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Кастрюли</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Мантоварки</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Сковороды</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Термосы</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Чайники и френч-прессы</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Сервировочная посуда</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Столовые приборы</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Бытовая техника</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Бытовая химия</Text>
+                <Text color='#333333' fontWeight='700' cursor='pointer' _active={{ color: '#E24C55' }}>Товары для дома и сада</Text>
+            </Box>
+            <Accordion allowMultiple defaultIndex={[0]}>
                 <AccordionItem>
                     <h2>
                         <AccordionButton>
-                            <Box as='span' flex='1' textAlign='left'>
-                                Section 1 title
+                            <Box as="span" flex="1" textAlign="left">
+                                Цена
                             </Box>
+                            <Image src={top} />
                         </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
+                    <AccordionPanel pb={4} display="flex" alignItems="center" justifyContent="center" gap="5px">
+                        <Input
+                            width="90px"
+                            bg="transparent"
+                            border="1px solid gray"
+                            borderRadius="0px"
+                            _hover="none"
+                            placeholder="750 ₽"
+                        />
+                        <AiOutlineMinus />
+                        <Input
+                            width="90px"
+                            bg="transparent"
+                            border="1px solid gray"
+                            borderRadius="0px"
+                            _hover="none"
+                            placeholder="12500 ₽"
+                        />
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
+
+            <Box width="230px">
+                <RangeSlider
+                    aria-label={["min", "max"]}
+                    defaultValue={value}
+                    min={0}
+                    max={15000}
+                    step={50}
+                    onChange={(val) => setValue(val)}
+                >
+                    <RangeSliderTrack
+                        height="4px"
+                    >
+                        <RangeSliderFilledTrack bg="gray.600" />
+                    </RangeSliderTrack>
+
+                    <RangeSliderThumb index={0}>
+                        <Box
+                            width="20px"
+                            height="30px"
+                            bg="red.500"
+                            borderRadius="md"
+                            _focus={{ boxShadow: "0 0 12px rgba(0, 0, 0, 0.8)" }}
+                        />
+                    </RangeSliderThumb>
+                    <RangeSliderThumb index={1}>
+                        <Box
+                            width="20px"
+                            height="30px"
+                            bg="red.500"
+                            borderRadius="md"
+                            _focus={{ boxShadow: "0 0 12px rgba(0, 0, 0, 0.8)" }}
+                        />
+                    </RangeSliderThumb>
+                </RangeSlider>
+
+                <Box display="flex" justifyContent="space-between" marginTop="8px">
+                    <Text>{value[0]} ₽</Text>
+                    <Text>{value[1]} ₽</Text>
+                </Box>
+            </Box>
         </Box>
+
     )
 }
